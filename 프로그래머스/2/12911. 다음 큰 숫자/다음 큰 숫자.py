89127c5d497e678b6e3d1286622d40d93c7379ec
hyunil-cho <jackandjills@naver.com>
2025-06-15 11:41:07 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.00 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,12 +1,9 @@
-# 가장 쉬운 방법은, 1을 더하는 것 -> 가장 아래 위치한 비트가 0일 경우
-# 가장 뒤에서 두 번째 비트가 0일 경우, 마지막이 1 이어도 1을 더하는 것이 유리함
-# 
+1
+2
+3
+4
+5
 def solution(n):
-    target_bin = bin(n)[2:].count("1")
-    next = n+1
-    while True:
-        next_bin = bin(next)[2:].count("1")
-        if target_bin == next_bin:
-            return next
-        next += 1
-    return next
+    pivot = n & -n;
+    before = ((n ^ (n + pivot)) // pivot) >> 2;
+    return (n + pivot) | before;
